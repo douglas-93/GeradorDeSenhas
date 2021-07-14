c7f1a386672507d3f06d1e35466d10c3bb853923
Douglas Andrade <dolts93@outlook.com>
2021-07-14 16:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeSenhas/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace GeradorDeSenhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nupLength = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.clbOptions = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nupLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(63, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gerador de Senhas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Personalizar senha";
+            // 
+            // nupLength
+            // 
+            this.nupLength.Location = new System.Drawing.Point(74, 92);
+            this.nupLength.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nupLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nupLength.Name = "nupLength";
+            this.nupLength.Size = new System.Drawing.Size(46, 23);
+            this.nupLength.TabIndex = 2;
+            this.nupLength.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tamanho";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "*Mínimo 1 e Máximo 50";
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbPassword.Location = new System.Drawing.Point(12, 167);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PlaceholderText = "Clique em \"Gerar Nova\"";
+            this.txbPassword.ReadOnly = true;
+            this.txbPassword.Size = new System.Drawing.Size(298, 23);
+            this.txbPassword.TabIndex = 9;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 196);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Gerar Nova";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(235, 196);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 11;
+            this.btnCopy.Text = "Copiar";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // clbOptions
+            // 
+            this.clbOptions.BackColor = System.Drawing.SystemColors.Window;
+            this.clbOptions.CheckOnClick = true;
+            this.clbOptions.FormattingEnabled = true;
+            this.clbOptions.Items.AddRange(new object[] {
+            "Letras maiúsculas",
+            "Letras minúsculas",
+            "Números",
+            "Simbolos"});
+            this.clbOptions.Location = new System.Drawing.Point(12, 121);
+            this.clbOptions.MultiColumn = true;
+            this.clbOptions.Name = "clbOptions";
+            this.clbOptions.Size = new System.Drawing.Size(248, 40);
+            this.clbOptions.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 231);
+            this.Controls.Add(this.clbOptions);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nupLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Senhas";
+            ((System.ComponentModel.ISupportInitialize)(this.nupLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nupLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.CheckedListBox clbOptions;
+    }
+}
+
---
+++ GeradorDeSenhas/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorDeSenhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txbPassword.Text))
+            {
+                MessageBox.Show("Gere uma senha primeiro", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                System.Windows.Forms.Clipboard.SetText(txbPassword.Text);
+                MessageBox.Show("Copiado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            List<string> lowerCase = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            List<string> upperCase = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            List<string> symbols = new List<string>() { "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "_", "`", "{", "|", "}", "~" };
+            List<string> numbers = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", };
+
+            List<string> passwordOptions = new List<string>();
+
+            foreach (var option in clbOptions.CheckedItems)
+            {
+                switch (option.ToString())
+                {
+                    case "Letras maiúsculas":
+                        passwordOptions.AddRange(upperCase);
+                        break;
+                    case "Letras minúsculas":
+                        passwordOptions.AddRange(lowerCase);
+                        break;
+                    case "Números":
+                        passwordOptions.AddRange(numbers);
+                        break;
+                    case "Simbolos":
+                        passwordOptions.AddRange(symbols);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Random random = new Random();
+            string temp = "";
+            for (int i = 0; i < nupLength.Value; i++)
+            {
+                int randomIndex = random.Next(passwordOptions.Count);
+                temp += passwordOptions[randomIndex];
+            }
+            txbPassword.Text = temp;
+        }
+    }
+}
